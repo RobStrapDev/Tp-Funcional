e06b27fd937e0ac0da8fee9b30ff719201599af9
Mariano Sosa <49418339+marianososaa@users.noreply.github.com>
2024-03-29 12:10:40 -0300

    Estructura de datos creada - REVISAR

--- TP-FUNCIONAL.hs
+++ TP-FUNCIONAL.hs
@@ -5,7 +5,6 @@
 -- Edad
 -- Recuerdos
 -- Viajes realizados
-
 data Viajero = Viajero {
     nombreViajero :: String,
     edadViajero :: Int,
@@ -18,20 +17,30 @@ data Viajero = Viajero {
 -- Tipo de viajes
 -- Transformaciones
 -- Año
-
 data Viaje = Viaje {
     destinoViaje :: String,
     tipoViaje :: TipoViaje,
-    transformacionesViaje :: [Transformaciones],
+    transformacionesViaje :: [Transformacion],
     anioViaje :: Int
 }
 
 -- Datos del recuerdo
 -- Nombre
 -- Proveniencia
-
 data Recuerdo = Recuerdo {
     nombreRecuerdo :: String,
     lugarProvenienciaRecuerdo :: String
 }
+
+-- Tipo de viajes
+-- Al pasado - Conocemos recuerdos
+-- Al futuro - Cantidad años que viajo
+data TipoViaje = AlFuturo Int | AlPasado [Recuerdo]
+
+-- Transformaciones
+-- Viaja al lejano oeste pierde todos los recuerdos que comienzan con vocal
+-- Viaja al futuro, Hill Valley, su edad aumenta
+data Transformacion = PerdidaRecuerdosVocales | AumentoEdad Int
+
+
 
